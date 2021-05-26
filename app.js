e8767e4aa0f85d285ce5064afddfbbe9f1f4f6d2
Braian <aldbraian@gmail.com>
2021-05-26 06:37:50 -0300

    Agregando dos comandos nuevos y mejoras en el codigo

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const { shootGameRandom } = require('./sort');
 
 const client = new Discord.Client();
 
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
-
 	console.log(command);
 	if (command === 'order') {
 		if (!args.length) {
@@ -24,17 +26,43 @@ client.on('message', message => {
 		const taggedUser = message.mentions.users.first();
 		message.channel.send(`You shot at ${taggedUser.username} and hit him/her in the ${shootGameRandom()}`);
 	}
-	else if (message.content.startsWith(`${prefix}whore`)) {
+	else if (command === 'whore') {
 		message.channel.send('Everyone’s a whore, Grace. We just sell different parts of ourselves.');
 	}
-	else if (message.content.startsWith(`${prefix}close`)) {
+	else if (command === 'close') {
 		message.channel.send('Oh… And there’s a woman. Yeah. A woman… I love… and I got close. I nearly got f****** EVERYTHING!');
 	}
-	else if (message.content === `${prefix}server`) {
+	else if (command === 'server') {
 		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\nCreated At: ${message.guild.createdAt}\nRegion: ${message.guild.region}`);
 	}
-	else if (message.content === `${prefix}user-info`) {
+	else if (command === 'user-info') {
 		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	}
+	else if (command === 'avatar') {
+		if (!message.mentions.users.size) {
+			return message.channel.send(`Do I look like a person that wants a simple life? ${message.author.displayAvatarURL({ format: 'png', dynamic: true })}`);
+		}
+		const avatarList = message.mentions.users.map(user => {
+			return `${user.username}'s avatar: ${user.displayAvatarURL({ format: 'png', dynamic: true })}`;
+		});
+		message.channel.send(avatarList);
+	}
+	else if (command === 'prune') {
+		if (!message.member.hasPermission('MANAGE_MESSAGES')) return;
+		const amount = parseInt(args[0]) + 1;
+		if (isNaN(amount)) {
+			return message.reply('that doesn\'t seem to be a valid number.');
+		}
+		else if (amount < 2 || amount > 100) {
+			return message.reply('you need to input a number between 2 and 100.');
+		}
+		message.channel.bulkDelete(amount, true).catch(err => {
+			console.log(err);
+			if (err.code === 50013) {
+				return message.channel.send('I can\'t prune messages because I don\'t have permissions');
+			}
+			message.channel.send('there was an error trying to prune messages in this channel!');
+		});
 	}
 });
 
